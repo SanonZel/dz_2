3ac6630de2c2b70b9c769f257f112b4543e14f0a
SanonZel <kanatovaha@gmail.com>
2022-12-13 15:56:06 +0600

    dz2

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Prosucts from './Prosucts'
+
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Prosucts/>
+    </div>
+  )
+}
+
+export default App
